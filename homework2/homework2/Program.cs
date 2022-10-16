b636c1cb3251090ca4a9327c77ac6ad2391c476c
tarasIvanov <65425491+tarasIvanov@users.noreply.github.com>
2022-10-17 01:32:45 +0300

    Homework 2

---
+++ homework2/homework2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace homework2
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+            int rows = 5;
+            int num = 1;
+            int i, j, n = 0;
+
+            
+            for (i = 0; i < rows; i++)
+            {
+                for (n = 1; n <= rows - i; n++)
+                    Console.Write(" ");
+                for (j = 0; j <= i; j++)
+                {
+                    if (j == 0 || i == 0)
+                        num = 1;
+                    else
+                        num = num * (i - j + 1) / j;
+                    Console.Write(num + " ");
+                }
+                Console.WriteLine();
+            }
+
+
+
+
+
+        }
+    }
+}
