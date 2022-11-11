56ad2f6a5ab35b1cd9836f51dcc40a3367a40bf8
Тарас Іванов <tarasivanov@MacBook-Pro-Taras.local>
2022-11-12 01:48:07 +0200

    Add prototypes of program

---
+++ sem_1_lab_1/Task2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int num = 0;
+    }
+
+    public static bool isSimple(int num)
+    {
+        return true;
+    }
+
+}
+
---
+++ sem_1_lab_1/task4.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        double sin1 = 0;
+        int sign = 1;
+        long f = 1;
+        int p = 1;
+        double x = 0;
+    }
+
+    public static long factorial(double x)
+    {
+        return endN;
+    }
+
+    public static double power(double x, double n)
+    {
+        return sum;
+    }
+}
+
