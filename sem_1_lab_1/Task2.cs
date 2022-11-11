15507bbf3138d228bf80080d078b6467ce9ad6de
Тарас Іванов <tarasivanov@MacBook-Pro-Taras.local>
2022-11-12 01:52:04 +0200

    full realisation

--- sem_1_lab_1/Task2.cs
+++ sem_1_lab_1/Task2.cs
@@ -4,10 +4,38 @@ class Program
     public static void Main(string[] args)
     {
         int num = 0;
+
+        Console.WriteLine("Enter num");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        if (num >= 1)
+        {
+            if (isSimple(num) == true)
+            {
+                Console.WriteLine("Num is simple");
+            } else
+            {
+                Console.WriteLine("Num is complex");
+            }
+
+        } else
+        {
+            Console.WriteLine("Enter num >= 1");
+        }
+
+
     }
 
     public static bool isSimple(int num)
     {
+        for (int i = 2; i < num; i++)
+        {
+            if (num % i == 0)
+            {
+                return false;
+            }
+        }
+
         return true;
     }
 
--- sem_1_lab_1/task1.cs
+++ sem_1_lab_1/task1.cs
@@ -11,5 +11,32 @@ class Program
         double dx = 0;
         double x = 0;
         double y = 0;
+
+
+        Console.WriteLine("Enter x0");
+        x0 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter xn");
+        xn = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter n");
+        n = Convert.ToDouble(Console.ReadLine());
+
+        x = x0;
+
+        dx = (xn - x0) / n;
+
+        for (double i = 0; i <= n; i++)
+        {
+            Console.WriteLine("x" + i + " = " + x);
+
+            y = x * Math.Sin(Math.Sqrt(x + b - 0.0084));
+
+            Console.WriteLine("y = " + y);
+
+            x += dx;
+        }
+
+
     }
 }
