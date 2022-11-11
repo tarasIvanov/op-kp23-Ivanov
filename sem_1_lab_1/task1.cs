309bbf42e20e4b08173d82d4210bc03afc4c184a
Тарас Іванов <tarasivanov@MacBook-Pro-Taras.local>
2022-11-12 01:48:07 +0200

    Add prototypes of program

---
+++ sem_1_lab_1/task1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Program
+{
+    const double b = 3.4;
+
+    public static void Main(string[] args)
+    {
+        double x0 = 0;
+        double xn = 0;
+        double n = 0;
+        double dx = 0;
+        double x = 0;
+        double y = 0;
+    }
+}
---
+++ sem_1_lab_1/task3.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int n, x = 0;
+        int sum = 1;
+        int prog = 1;
+    }
+}
