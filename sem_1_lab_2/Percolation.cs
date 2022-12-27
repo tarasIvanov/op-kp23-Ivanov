a884b4ca39ae75b001a18851f1169afe8fbc1e32
Тарас Іванов <tarasivanov@MacBook-Pro-Taras.local>
2022-12-27 23:41:54 +0200

    test cases + fixed bugs

--- sem_1_lab_2/Percolation.cs
+++ sem_1_lab_2/Percolation.cs
@@ -4,7 +4,7 @@ using System.Diagnostics.Metrics;
 
 class Percolation
 {
-    static public int n = 5;
+    static public int n = 10;
 
     static public int counter = 0;
     static public int pTop = 0;
@@ -17,17 +17,50 @@ class Percolation
 
     public static void Main(string[] args)
     {
-
         init(n);
 
-        //open(1, 3);
-        //open(2, 2);
-        //open(4, 4);
-        //open(3, 1);
-        //open(2, 3);
-        //open(2, 1);
+        //test case
 
-        randOpen();
+        //n = 10
+        //open(7, 3);
+        //open(8, 3);
+        //open(9, 3);
+        //open(10, 3);
+        //open(6, 4);
+        //open(6, 5);
+        //open(7, 5);
+        //open(8, 5);
+        //open(9, 5);
+        //open(10, 5);
+        //open(1, 7);
+        //open(2, 7);
+        //open(3, 7);
+        //open(4, 7);
+        //open(5, 7);
+        //open(6, 7);
+        //open(7, 7);
+        //open(7, 6);
+
+        //Output:
+
+        //0 0 0 0 0 0 2 0 0 0
+        //0 0 0 0 0 0 2 0 0 0
+        //0 0 0 0 0 0 2 0 0 0
+        //0 0 0 0 0 0 2 0 0 0
+        //0 0 0 2 2 0 2 0 0 0
+        //0 0 2 0 2 2 2 0 0 0
+        //0 0 2 0 2 0 0 0 0 0
+        //0 0 2 0 2 0 0 0 0 0
+        //0 0 2 0 2 0 0 0 0 0
+
+        //numberOfOpenSites = 18
+        //Percolates ? : True
+
+       
+
+
+
+        //randOpen();
 
         print();
         Console.WriteLine("\nnumberOfOpenSites = " + numberOfOpenSites());
@@ -201,6 +234,237 @@ class Percolation
 
         return rootX == rootY;
     }
+
+    static bool testOpen()
+    {
+        open(1, 3);
+        open(2, 3);
+        open(3, 3);
+        open(4, 3);
+        open(5, 3);
+
+        bool expected1 = true;
+        bool expected2 = true;
+        bool expected3 = true;
+        bool expected4 = true;
+        bool expected5 = true;
+
+        bool actual1 = isOpen(1, 3);
+        bool actual2 = isOpen(2, 3);
+        bool actual3 = isOpen(3, 3);
+        bool actual4 = isOpen(4, 3);
+        bool actual5 = isOpen(5, 3);
+
+        if (expected1 != actual1)
+        {
+            Console.WriteLine("TestOpen: case1 is failed");
+            return false;
+        }
+
+        if (expected2 != actual2)
+        {
+            Console.WriteLine("TestOpen: case2 is failed");
+            return false;
+        }
+
+        if (expected3 != actual3)
+        {
+            Console.WriteLine("TestOpen: case3 is failed");
+            return false;
+        }
+
+        if (expected4 != actual4)
+        {
+            Console.WriteLine("TestOpen: case4 is failed");
+            return false;
+        }
+
+        if (expected5 != actual5)
+        {
+            Console.WriteLine("TestOpen: case5 is failed");
+            return false;
+        }
+
+        Console.WriteLine("TestOpen: all cases passed");
+        return true;
+
+    }
+
+    static bool testIsOpen()
+    {
+        bool expected1 = true;
+        bool expected2 = false;
+
+        bool actual1 = isOpen(3, 3);
+        bool actual2 = isOpen(1, 1);
+
+        if (expected1 != actual1)
+        {
+            Console.WriteLine("TestIsOpen: case1 is failed");
+            return false;
+        }
+        if (expected2 != actual2)
+        {
+            Console.WriteLine("TestIsOpen: case2 is failed");
+            return false;
+        }
+        Console.WriteLine("TestIsOpen: all cases passed");
+        return true;
+    }
+
+    static bool testIsFull()
+    {
+        bool expected1 = true;
+        bool expected2 = false;
+
+        bool actual1 = isFull(3, 3);
+        bool actual2 = isFull(1, 1);
+
+        if (expected1 != actual1)
+        {
+            Console.WriteLine("TestIsFull: case1 is failed");
+            return false;
+        }
+        if (expected2 != actual2)
+        {
+            Console.WriteLine("TestIsFull: case2 is failed");
+            return false;
+        }
+        Console.WriteLine("TestIsFull: all cases passed");
+        return true;
+    }
+
+    static bool testNumberOfOpenSites()
+    {
+        int expected = 5;
+
+        int actual = numberOfOpenSites();
+
+        if (expected != actual)
+        {
+            Console.WriteLine("TestNumberOfOpenSites: case is failed");
+            return false;
+        }
+        Console.WriteLine("TestNumberOfOpenSites: case passed");
+        return true;
+    }
+
+    static bool testPercolates()
+    {
+        bool expected = true;
+
+        bool actual = percolates();
+
+        if (actual != expected)
+        {
+            Console.WriteLine("TestPecolates: case is failed");
+            return false;
+        }
+        Console.WriteLine("TestPercolates: case passed");
+        return true;
+    }
+
+    static bool testRoot()
+    {
+        int expected1 = 0;
+        int expected2 = 25;
+
+        int actual1 = root(3);
+        int actual2 = root(25);
+
+        if (expected1 != actual1)
+        {
+            Console.WriteLine("TestRoot: case1 is failed");
+            return false;
+        }
+        if (expected2 != actual2)
+        {
+            Console.WriteLine("TestRoot: case2 is failed");
+            return false;
+        }
+        Console.WriteLine("TestRoot: all cases passed");
+        return true;
+
+    }
+
+    static bool testUnion()
+    {
+        bool expected1 = false;
+        bool expected2 = true;
+
+        bool actual1 = connected(1, 2);
+        bool actual2 = connected(3, 8);
+
+        if (expected1 != actual1)
+        {
+            Console.WriteLine("TestUnion: case1 is failed");
+            return false;
+        }
+        if (expected2 != actual2)
+        {
+            Console.WriteLine("TestUnion: case2 is failed");
+            return false;
+        }
+        Console.WriteLine("TestUnion: all cases passed");
+        return true;
+    }
+
+    static bool testConnected()
+    {
+        bool expected1 = false;
+        bool expected2 = true;
+        bool actual1 = connected(1, 2);
+        bool actual2 = connected(3, 8);
+        if (expected1 != actual1)
+        {
+            Console.WriteLine("TestConnected: case1 is failed");
+            return false;
+        }
+        if (expected2 != actual2)
+        {
+            Console.WriteLine("TestConnected: case2 is failed");
+            return false;
+        }
+        Console.WriteLine("TestConnected: all cases passed");
+        return true;
+    }
+
+    static bool testGetIndex()
+    {
+        int expected1 = 1;
+        int expected2 = 25;
+        int expected3 = 8;
+        int expected4 = 11;
+
+        int actual1 = getIndex(1, 1);
+        int actual2 = getIndex(5, 5);
+        int actual3 = getIndex(2, 3);
+        int actual4 = getIndex(3, 1);
+
+        if (expected1 != actual1)
+        {
+            Console.WriteLine("TestGetIndex: case1 is failed");
+            return false;
+        }
+        if (expected2 != actual2)
+        {
+            Console.WriteLine("TestGetIndex: case2 is failed");
+            return false;
+        }
+        if (expected3 != actual3)
+        {
+            Console.WriteLine("TestGetIndex: case3 is failed");
+            return false;
+        }
+        if (expected4 != actual4)
+        {
+            Console.WriteLine("TestGetIndex: case4 is failed");
+            return false;
+        }
+        Console.WriteLine("TestGetIndex: all cases passed");
+        return true;
+    }
+
 
 }
 
