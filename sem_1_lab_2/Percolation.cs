643b821fba0a3c25a703df517897484f4bdd4401
Тарас Іванов <tarasivanov@MacBook-Pro-Taras.local>
2022-12-27 23:00:32 +0200

    test cases

---
+++ sem_1_lab_2/Percolation.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+
+class Percolation
+{
+    static public int n = 5;
+
+    static public int counter = 0;
+    static public int pTop = 0;
+    static public int pBottom = n * n + 1;
+
+    static bool[] blocked = new bool[(n * n) + 2];
+    static int[] size = new int[(n * n) + 2];
+    static int[] nodes = new int[(n * n) + 2];
+
+    public static void Main(string[] args)
+    {
+
+
+
+    }
+
+    static public void init(int n)
+    {
+        
+    }
+
+    static public void randOpen()
+    {
+        
+    }
+
+    static int getIndex(int row, int col)
+    {
+
+    }
+
+
+    static void open(int row, int col)
+    {
+        
+    }
+
+
+    static bool isOpen(int row, int col)
+    {
+
+    }
+
+
+    static bool isFull(int row, int col)
+    {
+
+    }
+
+    static public int numberOfOpenSites()
+    {
+
+    }
+
+    static bool percolates()
+    {
+
+    }
+
+
+    static void print()
+    {
+
+    }
+
+    static int root(int x)
+    {
+        
+    }
+
+    static void union(int x, int y)
+    {
+        
+    }
+
+    static bool connected(int x, int y)
+    {
+        
+    }
+
+}
+
+
