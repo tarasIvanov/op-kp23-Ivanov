fabc81cbc4859f81e350997cde9549587824cac9
tarasIvanov <taras.ivanov.ua@gmail.com>
2023-03-03 01:38:22 +0200

    task 2: full realisation

--- sem_2_lab_1/task2/Task 2.cs
+++ sem_2_lab_1/task2/Task 2.cs
@@ -2,16 +2,63 @@
 
 class Task2
 {
+	const int n = 15;
+
 	public static void Main(string[] args)
 	{
-		string pathMain;
-		string pathMax;
+		Random random = new Random();
+
+		string pathMain = Path.Combine(Directory.GetCurrentDirectory(), "task2.txt");
+		string pathMax = Path.Combine(Directory.GetCurrentDirectory(), "max.txt");
+
+		using (var sw = new StreamWriter(pathMain))
+		{
+			for (int i = 0; i < n; i++)
+			{
+				sw.Write(random.Next(1, 70) + " ");
+			}
+		}
+
+		using (var max = new StreamWriter(pathMax))
+		{
+			max.Write(GetMaxNumInFile(pathMain));
+		}
 	}
 
 	public static int GetMaxNumInFile(string pathMain)
 	{
-       
-    }
+       using (var sr = new StreamReader(pathMain))
+       {
+           int maxNumber = 0;
+
+           while (!sr.EndOfStream)
+           {
+               string numInStr = "";
+               char item = '\0';
+
+               while (true)
+               {
+                   item = (char)sr.Read();
+
+                   if (item == ' ')
+                   {
+                       break;
+                   }
+
+                   numInStr += item;
+               }
+
+               int digit = Convert.ToInt32(numInStr);
+
+               if (digit > maxNumber)
+               {
+                   maxNumber = digit;
+               }
+           }
+
+           return maxNumber;
+       }
+   }
 }
 
 
