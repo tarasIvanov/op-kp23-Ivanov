cd0ca46ed968847a477f5b7eecb612106b194fa0
tarasIvanov <taras.ivanov.ua@gmail.com>
2023-03-03 01:51:43 +0200

    task 4: prototype of program

---
+++ sem_2_lab_1/task4/Task4.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+class Task4
+{
+   const int numOfStudents;
+
+   public static void Main(string[] args)
+   {
+       string path;
+   }
+
+   public static void PrintStudentsLessSixtyPoints(string path)
+   {
+       
+   }
+}
+
