0a555bafea24e5e7eaa9413413813b8700b2f7ac
tarasIvanov <taras.ivanov.ua@gmail.com>
2023-03-03 01:52:22 +0200

    task 4: full realisation

--- sem_2_lab_1/task4/Task4.cs
+++ sem_2_lab_1/task4/Task4.cs
@@ -3,16 +3,72 @@ using System.IO;
 
 class Task4
 {
-   const int numOfStudents;
+   const int numOfStudents = 5;
 
    public static void Main(string[] args)
    {
-       string path;
+       Random random = new Random();
+
+       string path = Path.Combine(Directory.GetCurrentDirectory(), "Students.txt");
+
+       using (var sw = new StreamWriter(path))
+       {
+           for (int i = 0; i < numOfStudents; i++)
+           {
+               sw.WriteLine($"First name: {i + 1}, Last name: {i + 1}, " + random.Next(30, 100));
+           }
+       }
+
+       PrintStudentsLessSixtyPoints(path);
    }
+
 
    public static void PrintStudentsLessSixtyPoints(string path)
    {
-       
+       using (var sr = new StreamReader(path))
+       {
+           int counter = 0;
+           int studentsHaveLess = 0;
+
+           string student = "";
+           char el = '\0';
+           string score = "";
+
+           while (!sr.EndOfStream)
+           {
+
+               while (counter < 2)
+               {
+                   el = (char)sr.Read();
+                   student += el;
+
+                   if (el == ',')
+                   {
+                       counter++;
+                   }
+               }
+
+               el = (char)sr.Read();
+               student += el;
+
+               score = sr.ReadLine();
+               student += score;
+
+               if (Convert.ToInt32(score) < 60)
+               {
+                   Console.WriteLine(student + " --- this student has less than 60 points");
+                   studentsHaveLess++;
+               }
+               student = "";
+               counter = 0;
+           }
+
+           if (studentsHaveLess <= 0)
+           {
+               Console.WriteLine("0 students have less than 60");
+           }
+
+       }
    }
 }
 
