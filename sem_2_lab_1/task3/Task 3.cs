c9ae7f049dd3c71ba3c78612e0c3b33288cef76c
tarasIvanov <taras.ivanov.ua@gmail.com>
2023-03-03 01:40:46 +0200

    task 3: prototype of program

---
+++ sem_2_lab_1/task3/Task 3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Task3
+{
+    public static void Main(string[] args)
+    {
+        string[] words;
+        int numOfWords;
+        string pathBaseWords;
+        string pathChangedWords;
+    }
+
+    public static void SortArr(ref string[] words, int numOfWords)
+    {
+        
+    }
+
+}
+
+
