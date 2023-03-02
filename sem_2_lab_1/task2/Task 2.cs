a20adbd97f8239c0469a54437034ea4a7c3ecfa7
tarasIvanov <taras.ivanov.ua@gmail.com>
2023-03-03 01:37:37 +0200

    task 2: prototype of program

---
+++ sem_2_lab_1/task2/Task 2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Task2
+{
+	public static void Main(string[] args)
+	{
+		string pathMain;
+		string pathMax;
+	}
+
+	public static int GetMaxNumInFile(string pathMain)
+	{
+       
+    }
+}
+
+
