a88a5737b9a86b6d3783ad00a1881b79b86a1994
tarasIvanov <taras.ivanov.ua@gmail.com>
2023-03-03 01:34:04 +0200

    task 1: full realisation

--- sem_2_lab_1/task1/Task 1.cs
+++ sem_2_lab_1/task1/Task 1.cs
@@ -4,7 +4,21 @@ class Program
 {
    public static void Main(string[] args)
    {
-       string path;
+       string path = Path.Combine(Directory.GetCurrentDirectory(), "task1.txt");
+
+       using (var sw = new StreamWriter(path))
+       {
+           sw.WriteLine("Hello!!!");
+           sw.WriteLine("It's my solution of lab 1");
+       }
+
+       using (var sr = new StreamReader(path))
+       {
+           while (!sr.EndOfStream)
+           {
+               Console.WriteLine(sr.ReadLine());
+           }
+       }
    }
 
 }
