2b3ab03b6e5f5d3d5201d3b00377821aeae4319b
tarasIvanov <taras.ivanov.ua@gmail.com>
2023-03-03 15:49:14 +0200

    task 6: full realisation

--- sem_2_lab_1/task6/Task6.cs
+++ sem_2_lab_1/task6/Task6.cs
@@ -2,15 +2,117 @@
 
 class Task6
 {
-   public static void Main(string[] args)
-   {
-       int countOfStudents;
-       int numOfBestStudents;
+    public static void Main(string[] args)
+    {
+        Random random = new Random();
 
-       string pathTxtFile;
-       string pathBinaryStudents;
-       string pathBestStudents;
-   }
+        int countOfStudents = 0;
+        int numOfBestStudents = 0;
+
+        string pathTxtFile = Path.Combine(Directory.GetCurrentDirectory(), "Students.txt");
+        string pathBinaryStudents = Path.Combine(Directory.GetCurrentDirectory(), "Binary_Students.dat");
+        string pathBestStudents = Path.Combine(Directory.GetCurrentDirectory(), "BestStudents.dat");
+
+        using (var sr = new StreamReader(pathTxtFile))
+        {
+            var bw = new BinaryWriter(File.Open(pathBinaryStudents, FileMode.OpenOrCreate));
+
+            string str;
+
+            while (!sr.EndOfStream)
+            {
+                str = sr.ReadLine();
+
+                try
+                {
+                    //
+                    Console.WriteLine(str);
+                    bw.Write(str);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message + "Can't write to file");
+                }
+
+                countOfStudents++;
+            }
+
+            //
+            Console.WriteLine();
+            bw.Close();
+        }
+
+        using (var bw = new BinaryWriter(File.Open(pathBestStudents, FileMode.OpenOrCreate)))
+        {
+            var br = new BinaryReader(File.Open(pathBinaryStudents, FileMode.OpenOrCreate));
+
+            string str = "";
+            char el = '\0';
+            int score = 0;
+            string scoreStr = "";
+
+            int counter = 0;
+            int countOfSpases = 0;
+            int countOfNumbers = 0;
+
+            while (counter < countOfStudents)
+            {
+                do
+                {
+                    el = br.ReadChar();
+                    str += el;
+
+                    if (el == ' ')
+                    {
+                        countOfSpases++;
+                    }
+
+                } while (countOfSpases < 6);
+
+                do
+                {
+                    el = br.ReadChar();
+
+                    scoreStr += el;
+
+                    countOfNumbers++;
+                } while (countOfNumbers < 2);
+
+                str += scoreStr;
+
+                score = Convert.ToInt32(scoreStr);
+
+                br.Read();
+
+                if (score >= 95)
+                {
+                    numOfBestStudents++;
+
+                    Console.WriteLine(str);
+                    bw.Write(str);
+                }
+
+                scoreStr = "";
+                str = "";
+                score = 0;
+                countOfNumbers = 0;
+                countOfSpases = 0;
+
+
+                counter++;
+            }
+
+            if (numOfBestStudents > 0)
+            {
+                Console.Write("Num of best students is: " + numOfBestStudents);
+                bw.Write("Num of best students is: " + numOfBestStudents);
+            }
+
+            br.Close();
+        }
+
+
+    }
 
 }
 
