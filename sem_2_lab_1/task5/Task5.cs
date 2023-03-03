70dab867fcd304f9e44cf662f5f4291b44ab3619
tarasIvanov <taras.ivanov.ua@gmail.com>
2023-03-03 15:36:02 +0200

    task 5: prototype of program

---
+++ sem_2_lab_1/task5/Task5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Task5
+{
+   public static void Main(string[] args)
+   {
+       int n;
+
+       string[] words = new string[n];
+       int[] numOfRepetition = new int[n];
+
+       string path;
+
+       char el;
+       string word;
+       int counter;
+   }
+
+   public static void Resize(ref string[] words, ref int[] numOfRepetition, ref int n, int addToLength = 20)
+   {
+       
+   }
+
+}
