c750ce4212180a14d118665ad2539dc821c841e3
tarasIvanov <taras.ivanov.ua@gmail.com>
2023-03-03 15:48:25 +0200

    task 6: prototype of program

---
+++ sem_2_lab_1/task6/Task6.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Task6
+{
+   public static void Main(string[] args)
+   {
+       int countOfStudents;
+       int numOfBestStudents;
+
+       string pathTxtFile;
+       string pathBinaryStudents;
+       string pathBestStudents;
+   }
+
+}
+
+
