aae2ce7061d2c1ff0ea514ad080dad707a41a1ff
tarasIvanov <taras.ivanov.ua@gmail.com>
2023-03-03 15:37:05 +0200

    task 5: full realisation

--- sem_2_lab_1/task5/Task5.cs
+++ sem_2_lab_1/task5/Task5.cs
@@ -2,23 +2,98 @@
 
 class Task5
 {
-   public static void Main(string[] args)
-   {
-       int n;
+    const int beginLettersInASCII = 65;
+    const int endElttersInASCII = 122;
 
-       string[] words = new string[n];
-       int[] numOfRepetition = new int[n];
+    const int size = 5;
 
-       string path;
+    public static void Main(string[] args)
+    {
+        string[] words = new string[size];
+        int[] numOfRepetition = new int[size];
 
-       char el;
-       string word;
-       int counter;
-   }
+        string path = Path.Combine(Directory.GetCurrentDirectory(), "text_task5.txt");
 
-   public static void Resize(ref string[] words, ref int[] numOfRepetition, ref int n, int addToLength = 20)
-   {
-       
-   }
+        char el = '\0';
+        string word = "";
+        int counter = 0;
+
+        using (var sr = new StreamReader(path))
+        {
+            while (!sr.EndOfStream)
+            {
+                do
+                {
+                    el = (char)sr.Read();
+
+                    if ((int)el < beginLettersInASCII || (int)el > endElttersInASCII)
+                    {
+                        break;
+                    }
+
+                    word += el;
+                } while ((int)el > beginLettersInASCII && (int)el < endElttersInASCII);
+
+
+                for (int i = 0; i <= counter; i++)
+                {
+                    if (word == words[i])
+                    {
+                        numOfRepetition[i]++;
+                        break;
+                    }
+
+                    if (i == counter && word != "")
+                    {
+                        words[counter] = word;
+                        numOfRepetition[counter]++;
+                        counter++;
+                        break;
+                    }
+                }
+
+
+                if (counter == words.Length - 1)
+                {
+                    Resize(ref words, ref numOfRepetition, ref size);
+                }
+
+                word = "";
+            }
+
+        }
+
+        for (int i = 0; i < counter; i++)
+        {
+            Console.WriteLine(words[i] + ": " + numOfRepetition[i]);
+        }
+
+    }
+
+    public static void Resize(ref string[] words, ref int[] numOfRepetition, ref int n, int addToLength = 20)
+    {
+        //Edit n
+        n += addToLength;
+
+        //Edit arr of words
+        string[] tempArr = new string[words.Length + addToLength];
+
+        for (var i = 0; i < words.Length; i++)
+        {
+            tempArr[i] = words[i];
+        }
+
+        words = tempArr;
+
+        //Edit arr of numOfRepetition
+        int[] tempArr2 = new int[numOfRepetition.Length + addToLength];
+
+        for (var i = 0; i < numOfRepetition.Length; i++)
+        {
+            tempArr2[i] = numOfRepetition[i];
+        }
+
+        numOfRepetition = tempArr2;
+    }
 
 }
