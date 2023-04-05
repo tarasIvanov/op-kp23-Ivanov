75e22f537f2f8b677131ef97b3daf80dde73ef03
tarasIvanov <taras.ivanov.ua@gmail.com>
2023-04-06 02:30:55 +0300

    2.3 full realization

---
+++ sem_2_lab_2/task 2/task 2.3/IVessel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ForAll.task2
+{
+	public interface IVessel
+	{
+        public void PrepareToMovement();
+        public void Move();
+    }
+}
+
---
+++ sem_2_lab_2/task 2/task 2.3/Submarine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace ForAll.task2
+{
+    class Submarine : IVessel
+    {
+        public void Move()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Підводний човен поїхав");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public void PrepareToMovement()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Підводний човен готується до руху");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
+
