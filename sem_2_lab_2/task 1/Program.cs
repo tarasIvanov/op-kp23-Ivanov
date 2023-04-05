897f66953a161dfaa12a4d85b55c0701df3bf51f
tarasIvanov <taras.ivanov.ua@gmail.com>
2023-04-06 00:27:09 +0300

    full realization

---
+++ sem_2_lab_2/task 1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Globalization;
+
+namespace ForAll
+{
+    class Program
+    {
+        const int n = 10;
+
+        public static void Main(string[] args)
+        {
+            SalaryNote[] salaryNotes = new SalaryNote[n];
+
+            int counter = 0;
+
+            int digit = 0;
+
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("1)Create new note");
+                Console.WriteLine("2)Show notes");
+                Console.WriteLine("3)End the program");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.WriteLine("Enter digit:");
+                digit = int.Parse(Console.ReadLine());
+
+                switch (digit)
+                {
+                    case 1:
+                        salaryNotes[counter] = CreateNewNote();
+                        counter++;
+                        break;
+                    case 2:
+                        if (counter == 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Magenta;
+                            Console.WriteLine("Empty");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            continue;
+                        }
+
+                        for (int i = 0; i < counter; i++)
+                        {
+                            salaryNotes[i].PrintCharacteristics();
+                        }
+                        break;
+                    case 3:
+                        return;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Wrong digit");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                }
+            }
+        }
+
+        public static SalaryNote CreateNewNote()
+        {
+            SalaryNote salaryNote = new SalaryNote();
+            salaryNote.SetParams();
+
+            return salaryNote;
+        }
+    }
+}
+
---
+++ sem_2_lab_2/task 1/SalaryNote.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace ForAll
+{
+	class SalaryNote
+	{
+		public string Name { get; set; }
+		public int Salary { get; set; }
+		public int Withheld { get; set; } //Утримано
+		public int Received { get; set; }
+
+		public SalaryNote(string name, int salary, int withheld)
+		{
+			Name = name;
+			Salary = salary;
+			Withheld = withheld;
+
+			ReceivedMoney();
+		}
+
+        public SalaryNote()
+		{
+
+		}
+
+        private void ReceivedMoney()
+		{
+			Received = Salary - Withheld;
+        }
+
+        public void SetParams()
+        {
+			Console.WriteLine("Enter name");
+            Name = Console.ReadLine();
+
+			Console.WriteLine("Enter Salary");
+            Salary = int.Parse(Console.ReadLine());
+
+			Console.WriteLine("Enter withheld");
+            Withheld = int.Parse(Console.ReadLine());
+
+            ReceivedMoney();
+        }
+
+        public void SetParams(string name, int salary, int withheld)
+		{
+			Name = name;
+			Salary = salary;
+			Withheld = withheld;
+
+            ReceivedMoney();
+        }
+
+		public void PrintCharacteristics()
+		{
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Name: {Name}\tSalary: {Salary}\tWithheld: {Withheld}\tReceived: {Received}");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
+
