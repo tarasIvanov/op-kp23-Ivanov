4340fbd5a09453f4680ea546296c0cfbab74466c
tarasIvanov <taras.ivanov.ua@gmail.com>
2023-05-21 23:58:12 +0300

    stub level of program

---
+++ sem_2_lab_3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Globalization;
+
+namespace ForAll
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Deque<int> deque = new Deque<int>();
+        }
+    }
+}
+
