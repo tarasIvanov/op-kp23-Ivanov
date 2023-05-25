0b1444f9a7ef97dc26ef4fc5b61588f0cc955c33
tarasIvanov <taras.ivanov.ua@gmail.com>
2023-05-26 02:22:08 +0300

    task2.1: full realisation

---
+++ sem_2_lab_3/Task 2/RandomizedQueueArr.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ForAll
+{
+	public class RandomizedQueue<Item> : IIterator<Item>
+	{
+        Random random = new Random();
+
+        public bool HasNext { get; private set; } = true;
+
+        private Item[] arr;
+
+        private int Count = 0;
+        private int CapaciryOfArr;
+
+        private bool[] arrOfRepeatItems;
+        private Item _currentItem;
+
+        private int counterIterator = 0;
+
+        // construct an empty randomized queue
+        public RandomizedQueue(int sizeOfArr)
+        {
+            arr = new Item[sizeOfArr];
+        }
+
+        // is the randomized queue empty?
+        public bool IsEmpty()
+        {
+            if (Count == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        // return the number of items on the randomized queue
+        public int Size()
+        {
+            return Count;
+        }
+
+        // add the item
+        public void Enqueue(Item item)
+        {
+            if (Count >= CapaciryOfArr)
+            {
+                Resize(ref arr, ref CapaciryOfArr);
+            }
+
+            arr[Count] = item;
+
+            Count++;
+        }
+
+        // remove and return a random item
+        public Item Dequeue()
+        {
+            if (Count <= 0)
+            {
+                return default!;
+            }
+
+            int randomNum = random.Next(0, Count);
+            Item tempItem = arr[randomNum];
+
+            var tmp = new List<Item>(arr);
+            tmp.RemoveAt(randomNum);
+            arr = tmp.ToArray();
+
+            Count--;
+
+            return tempItem;
+        }
+
+        // return a random item (but do not remove it)
+        public Item Sample()
+        {
+            return arr[random.Next(0, Count)];
+        }
+
+        // return an independent iterator over items in random order
+        public IIterator<Item> iterator()
+        {
+            arrOfRepeatItems = new bool[Count];
+
+            return this;
+        }
+
+        Item IIterator<Item>.MoveNext()
+        {
+            if (counterIterator >= Count)
+            {
+                this.HasNext = false;
+
+                return _currentItem;
+            }
+
+            int randomNum = random.Next(0, Count);
+
+            if (counterIterator == 0)
+            {
+                _currentItem = arr[randomNum];
+                arrOfRepeatItems[randomNum] = true;
+
+                counterIterator++;
+            }
+
+            while (true)
+            {
+                if (arrOfRepeatItems[randomNum])
+                {
+                    randomNum = random.Next(0, Count);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            var temp = _currentItem;
+
+            _currentItem = arr[randomNum];
+
+            arrOfRepeatItems[randomNum] = true;
+
+            counterIterator++;
+
+            return temp;
+        }
+
+        private static void Resize(ref Item[] arr, ref int CapaciryOfArr, int addToLength = 10)
+        {
+            CapaciryOfArr += 20;
+
+            Item[] tempArr = new Item[arr.Length + addToLength];
+
+            for (var i = 0; i < arr.Length; i++)
+            {
+                tempArr[i] = arr[i];
+            }
+
+            arr = tempArr;
+        }
+    }
+}
+
--- sem_2_lab_3/Task 2/Task 2.cs
+++ sem_2_lab_3/Task 2/Task 2.cs
@@ -9,10 +9,70 @@ namespace ForAll
         public static void Main(string[] args)
         {
             RandomizedQueue<int> queue = new RandomizedQueue<int>(10);
+
+            queue.Enqueue(5);
+            queue.Enqueue(3);
+            queue.Enqueue(2);
+            queue.Enqueue(7);
+            queue.Enqueue(10);
+
+            Console.WriteLine("-:" + queue.Sample());
+            Console.WriteLine("-:" + queue.Sample());
+
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Dequeue();
+
+            Console.WriteLine("-:" + queue.Sample());
+            Console.WriteLine("-:" + queue.Sample());
+
+            Console.WriteLine("-:" + queue.Sample());
+
+            IIterator<int> iterator = queue.iterator();
+
+            while (iterator.HasNext)
+            {
+                Console.WriteLine(iterator.MoveNext());
+            }
+
+            //Console.WriteLine("Unit test: " + UnitTest());
+
         }
 
         public static bool UnitTest()
         {
+            const int size = 4;
+
+            string[] expectedArr = { "string 4", "string 3", "string 2", "string 1" };
+
+            string[] arr = new string[size];
+            int counter = 0;
+
+            bool res = true;
+
+            DequeForArr<string> deque2 = new DequeForArr<string>(5);
+
+            deque2.AddFirst("string 2");
+            deque2.AddLast("string 3");
+            deque2.AddLast("string 4");
+            deque2.AddFirst("string 1");
+
+            IIterator<string> iterator = deque2.iterator();
+
+            while (iterator.HasNext)
+            {
+                arr[counter] = iterator.MoveNext();
+                counter++;
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                if (arr[i] != expectedArr[i])
+                {
+                    res = false;
+                }
+            }
+
             return res;
         }
     }
