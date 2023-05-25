bfc9ae09f7913ec8a48060a9d31d2aeecb68b038
tarasIvanov <taras.ivanov.ua@gmail.com>
2023-05-25 22:06:17 +0300

    full realisation + unit test

---
+++ sem_2_lab_3/Task 1/DequeForArr.cs
@@ -0,0 +1,161 @@
+﻿using System;
+namespace ForAll
+{
+    public class DequeForArr<Item> : IIterator<Item>
+    {
+        public bool HasNext { get; private set; } = true;
+
+        private Item[] arr;
+
+        private int Count = 0;
+        private int CapaciryOfArr;
+
+        Item _currentItem;
+
+        private int counterIterator = 0;
+
+        // construct an empty deque
+        public DequeForArr(int sizeOfArr)
+        {
+            arr = new Item[sizeOfArr];
+
+            this.CapaciryOfArr = sizeOfArr;
+        }
+
+        // is the deque empty?
+        public bool isEmpty()
+        {
+            if (Count == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        // return the number of items on the deque
+        public int GetSize()
+        {
+            return Count;
+        }
+
+        // add the item to the front
+        public void AddFirst(Item item)
+        {
+            if (Count >= CapaciryOfArr)
+            {
+                Resize(ref arr, ref CapaciryOfArr);
+            }
+
+            for (int i = Count; i > 0; i--)
+            {
+                arr[i] = arr[i - 1];
+            }
+
+            arr[0] = item;
+
+            Count++;
+        }
+
+        // add the item to the back
+        public void AddLast(Item item)
+        {
+            if (Count >= CapaciryOfArr)
+            {
+                Resize(ref arr, ref CapaciryOfArr);
+            }
+
+            arr[Count] = item;
+
+            Count++;
+        }
+
+        // remove and return the item from the front
+        public Item RemoveFirst()
+        {
+            if (Count > 0)
+            {
+                Item temp = arr[0];
+
+                for (int i = 0; i < Count; i++)
+                {
+                    arr[i] = arr[i + 1];
+                }
+
+                arr[Count - 1] = default!;
+
+                Count--;
+
+                return temp;
+            }
+
+            return default!;
+        }
+
+        // remove and return the item from the back
+        public Item RemoveLast()
+        {
+            if (Count > 0)
+            {
+                Item temp = arr[Count - 1];
+
+                arr[Count - 1] = default!;
+
+                Count--;
+
+                return temp;
+            }
+
+            return default!;
+        }
+
+        //// return an iterator over items in order from front to back
+        public IIterator<Item> iterator()
+        {
+            return this;
+        }
+
+        Item IIterator<Item>.MoveNext()
+        {
+            if (counterIterator == 0)
+            {
+                _currentItem = arr[Count - 1];
+
+                counterIterator++;
+            }
+
+            if (counterIterator >= Count)
+            {
+                this.HasNext = false;
+
+                return _currentItem;
+            }
+
+            var temp = _currentItem;
+
+            _currentItem = arr[Count - counterIterator - 1];
+
+            counterIterator++;
+
+            return temp;
+        }
+
+
+        private static void Resize(ref Item[] arr, ref int CapaciryOfArr, int addToLength = 10)
+        {
+            CapaciryOfArr += 20;
+
+            Item[] tempArr = new Item[arr.Length + addToLength];
+
+            for (var i = 0; i < arr.Length; i++)
+            {
+                tempArr[i] = arr[i];
+            }
+
+            arr = tempArr;
+        }
+    }
+
+
+}
+
---
+++ sem_2_lab_3/Task 1/Task1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using System.Globalization;
+
+namespace ForAll
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //DequeForList<int> deque1 = new DequeForList<int>();
+
+            //deque1.AddLast(5);
+            //deque1.AddLast(5);
+            //deque1.AddFirst(1);
+            //deque1.AddLast(2);
+            //deque1.AddLast(3);
+            //deque1.AddFirst(1);
+
+            //DequeForArr<string> deque2 = new DequeForArr<string>(5);
+
+            //deque2.AddFirst("string 2");
+            //deque2.AddLast("string 3");
+            //deque2.AddLast("string 4");
+            //deque2.AddFirst("string 1");
+
+            //IIterator<string> iterator = deque2.iterator();
+
+            //while (iterator.HasNext)
+            //{
+            //    Console.WriteLine(iterator.MoveNext());
+            //}
+
+            Console.WriteLine("Unit test: " + UnitTest());
+        }
+
+        public static bool UnitTest()
+        {
+            const int size = 4;
+
+            string[] expectedArr = { "string 4", "string 3", "string 2", "string 1" };
+
+            string[] arr = new string[size];
+            int counter = 0;
+
+            bool res = true;
+
+            DequeForArr<string> deque2 = new DequeForArr<string>(5);
+
+            deque2.AddFirst("string 2");
+            deque2.AddLast("string 3");
+            deque2.AddLast("string 4");
+            deque2.AddFirst("string 1");
+
+            IIterator<string> iterator = deque2.iterator();
+
+            while (iterator.HasNext)
+            {
+                arr[counter] = iterator.MoveNext();
+                counter++;
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                if (arr[i] != expectedArr[i])
+                {
+                    res = false;
+                }
+            }
+
+            return res;
+        }
+    }
+}
