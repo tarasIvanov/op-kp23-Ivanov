8004115e604b0d4a729faadffe02429e451a9bb2
tarasIvanov <taras.ivanov.ua@gmail.com>
2023-06-04 00:12:15 +0300

    stub level of program

---
+++ sem_2_lab_4/HashTable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace ForAll
+{
+	class HashTable<KItem, VItem>
+    {
+        public HashTable()
+		{
+
+		}
+
+        public HashTable(int intialCapacity)
+        {
+
+        }
+
+        public void Add(KItem key, VItem value)
+        {
+            
+        }
+
+        public void Remove(KItem key)
+        {
+            
+        }
+
+        public VItem Get(KItem key)
+        {
+
+            return default!;
+        }
+
+        public bool Contains(VItem key)
+        {
+            return default!;
+
+        }
+
+        public void clear()
+        {
+            
+        }
+
+        public int size()
+        {
+            return default!;
+        }
+
+    }
+}
+
